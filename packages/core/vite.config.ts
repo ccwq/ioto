93c983103accd1e0eac0b31bd92b826cbef32635
ccwq <scwqd@qq.com>
2022-11-20 11:11:01 +0800

    chore: 增加注释

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import {resolve} from "path";
 
 //@ts-ignore
@@ -10,45 +10,49 @@ const isDev = process.env.NODE_ENV === 'development'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    dts({
-      outputDir: resolve(__dirname, 'types'),
-    })
-  ],
-  build:{
-    lib:{
-      entry: resolve(__dirname, 'src/index.ts'),
+    plugins: [
+        dts({
+            outputDir: resolve(__dirname, 'types'),
+        })
+    ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
 
-      // Option "build.src.name" is required when output formats include "umd" or "iife".
-      name: 'index',
-      // formats: ['es'],
-      // the proper extensions will be added
-      fileName: 'index'
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        globals: {
-          // vue: 'Vue'
+            // Option "build.src.name" is required when output formats include "umd" or "iife".
+            name: 'index',
+            // formats: ['es'],
+            // the proper extensions will be added
+            fileName: 'index'
+        },
+
+        // 确保外部化处理那些你不想打包进库的依赖
+        rollupOptions: {
+            external: [],
+            output: {
+
+                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                globals: {
+                    // vue: 'Vue'
+                }
+            }
         }
-      }
-    }
-  },
+    },
 
-  define: {
+    define: {
 
-    // 一处vitest的测试代码
-    ...isDev?{}:{
-      'import.meta.vitest': 'undefined'
-    }
-  },
+        // 一处vitest的测试代码
+        ...isDev ? {} : {
+            'import.meta.vitest': 'undefined'
+        }
+    },
 
-  test:{
+    test: {
 
-    // 文件内测试
-    // includeSource: ['src/**/*.{js,ts}'],
+        // 文件内测试
+        // includeSource: ['src/**/*.{js,ts}'],
 
-    // 解决类似windows is not define问题
-    environment:"jsdom",
-  },
+        // 解决类似windows is not define问题
+        environment: "jsdom",
+    },
 })
