cce9f7e566cf9d12b9afb3b4f4cdff53326434bd
ccwq <scwqd@qq.com>
2024-05-23 10:34:18 +0800

    v0.1.15

---
+++ packages/vue/src/components/RouterAnchor.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+RouterLink.RouterAnchor(:class="classes"): slot
+
+
+</template>
+<script lang="ts" setup>
+import {computed, useAttrs} from "vue"
+import {useRoute} from "vue-router";
+
+
+/**
+ * 允许一个router-link 在其他路由处于激活状态
+ * @example - 比如要自新闻的详情页面,让新闻的导航页处于高亮状态
+ * (route)=>{
+ *     return route.name == "detail" && route.params?.type=="news"
+ * }
+ *
+ */
+const props = withDefaults(defineProps<{
+    // activeInDetailType: string[],
+
+    activeFunc?: (route:ReturnType<typeof useRoute>)=>boolean,
+
+}>(),{})
+const emits = defineEmits<{}>()
+const attrs = useAttrs()
+const route = useRoute()
+const classes = computed(()=>{
+    const params = route.params?.type
+    const path = route.path;
+    const ret = {} as any
+    if (props?.activeFunc?.(route)) {
+        const key = attrs.activeClass as string || "router-link-active";
+        ret[key] = true;
+    }
+    return ret;
+})
+
+
+</script>
+<style scoped lang="less">
+.RouterAnchor{
+
+}
+</style>
